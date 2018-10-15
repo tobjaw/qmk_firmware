75adb960966f75e26b6790ce9f189731aa2d28b5
tobjaw <tobjaw@gmail.com>
2018-10-15 16:58:12 +0200

    feat(dz60): define bootloader

--- keyboards/dz60/rules.mk
+++ keyboards/dz60/rules.mk
@@ -1,3 +1,9 @@
+# Bootloader
+#     This definition is optional, and if your keyboard supports multiple bootloaders of
+#     different sizes, comment this out, and the correct address will be loaded
+#     automatically (+60). See bootloader.mk for all options.
+BOOTLOADER = atmel-dfu
+
 # MCU name
 MCU = atmega32u4
 
